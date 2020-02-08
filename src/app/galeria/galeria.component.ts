9c50530e376afcdbbe7a0f9057156775ccb44678
KamiiGelp <kamiigelp22@gmail.com>
2020-02-08 02:47:28 -0300

    PRIMEROS  CAMBIOS EN EL DISEÑO DE LA VISTA POKEMON

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -64,8 +64,8 @@ export class GaleriaComponent implements OnInit {
   public openDialog(id): void {
     this.pokemonApi.cargarId(id);
     const dialogRef = this.dialog.open(PokemonComponent, {
-      width: '550px',
-      height: '400px',
+      width: '600px',
+      height: '650px',
     });
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
